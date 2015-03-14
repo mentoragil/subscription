b27140574b9250e58d2847a80d544ef7e821d2e9
Mentor Ágil <mentoragil@gmail.com>
2015-03-13 23:45:20 -0300

    removing secrets

--- subscription/views.py
+++ subscription/views.py
@@ -6,8 +6,7 @@ import logging
 import requests
 import sys
 
-MAILCHIMP_LIST_ID = '17edfa12f6'
-#API_KEY = '3a0e977cf36db4fe9a4c46eb3b9272d4-us10'
+MAILCHIMP_LIST_ID = 'secret'
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -17,9 +16,6 @@ def subscribe(request):
     if request.POST['email']:
         try:
             email_address = request.POST['email']
-            #data = '{ "apikey": "' + API_KEY + '", "id": "' + MAILCHIMP_LIST_ID + '", "email": { "email": "' + email_address + '" }, "merge_vars": {}, "email_type": "html", "double_optin": true, "update_existing": true, "replace_interests": true, "send_welcome": true }'
-            #response = requests.post('https://us10.api.mailchimp.com/2.0/lists/Subscribe.json', data=data, headers={'Content-type': 'application/json', 'Accept': 'text/plain'}, verify=False).text
-            #print >>sys.stderr, response
             list = utils.get_connection().get_list_by_id(MAILCHIMP_LIST_ID)
             list.subscribe(email_address, {'EMAIL': email_address})
             return HttpResponseRedirect('/subscription/success')
